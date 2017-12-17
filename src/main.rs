d3aa8a4697fbcfa5141be28366580c4c8fd1d33b
Phoomparin Mano <work@phoom.in.th>
2017-12-17 14:30:59 +0700

    :sparkles: Changed Currency Converter to use Structs instead

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,16 @@
 use std::ops;
+use std::fmt;
 
-#[derive(Debug)]
-#[derive(Clone)]
-pub enum Currency {
-  Dollar,
-  Baht,
-  Bitcoin,
+#[derive(Debug, Clone)]
+pub struct Currency {
+  name: &'static str,
+  rate_to_dollar: f32,
+  rate_from_dollar: f32,
 }
 
-impl Currency {
-  // The amount of dollar per one bucks in that currency
-  fn rate_to_dollar(&self) -> f32 {
-    match *self {
-      Currency::Dollar => 1.0,
-      Currency::Baht => 0.031,
-      Currency::Bitcoin => 19519.0
-    }
-  }
-
-  // The amount of currency a dollar can buy
-  fn rate_from_dollar(&self) -> f32 {
-    match *self {
-      Currency::Dollar => 1.0,
-      Currency::Baht => 32.51,
-      Currency::Bitcoin => 0.000051
-    }
+impl fmt::Display for Currency {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}", self.name)
   }
 }
 
@@ -34,6 +20,30 @@ struct Cash {
   amount: f32,
 }
 
+impl fmt::Display for Cash {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{} {}", self.amount, self.currency)
+  }
+}
+
+pub const USD: Currency = Currency {
+  name: "USD",
+  rate_to_dollar: 1.0,
+  rate_from_dollar: 1.0,
+};
+
+pub const THB: Currency = Currency {
+  name: "THB",
+  rate_to_dollar: 0.031,
+  rate_from_dollar: 32.51,
+};
+
+pub const BTC: Currency = Currency {
+  name: "BTC",
+  rate_to_dollar: 19519.0,
+  rate_from_dollar: 0.000051,
+};
+
 impl Cash {
   pub fn new(amount: f32, currency: Currency) -> Cash {
     Cash { amount, currency }
@@ -41,10 +51,10 @@ impl Cash {
 
   fn convert(&self, currency: Currency) -> Cash {
     // Converts our currency to dollar
-    let amount_in_dollar = self.amount * self.currency.rate_to_dollar();
+    let amount_in_dollar = self.amount * self.currency.rate_to_dollar;
 
     // Convert the dollar to our target currency
-    let amount_in_currency = amount_in_dollar * currency.rate_from_dollar();
+    let amount_in_currency = amount_in_dollar * currency.rate_from_dollar;
 
     Cash::new(amount_in_currency, currency)
   }
@@ -62,9 +72,9 @@ impl ops::Add<Cash> for Cash {
 }
 
 fn main() {
-  let my_cash = Cash::new(1.0, Currency::Bitcoin);
-  let your_cash = Cash::new(30000.0, Currency::Baht);
+  let my_cash = Cash::new(1.0, BTC);
+  let your_cash = Cash::new(30000.0, THB);
   let total_cash = my_cash + your_cash;
 
-  println!("{:?}", total_cash);
+  println!("I have {}", total_cash.convert(USD));
 }
